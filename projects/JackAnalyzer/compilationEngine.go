51e80a298f7503e1d70cc0c2c2a29e0b5df6de95
cashew <42924021+cashev@users.noreply.github.com>
2021-02-15 00:23:38 +0900

    xml出力処理をリファクタリング

--- projects/JackAnalyzer/compilationEngine.go
+++ projects/JackAnalyzer/compilationEngine.go
@@ -3,56 +3,59 @@ package main
 var position int
 var tokens []token
 
-func compile(toks []token) []string {
+var results []string
+
+func initializeCompileEngine() {
 	position = 0
+	tokens = make([]token, 0)
+	results = make([]string, 0)
+}
+
+func compile(toks []token) []string {
+	initializeCompileEngine()
 	tokens = toks
 
-	var results []string
 	for position+1 < len(tokens) {
 		tok := tokens[position]
-		results = append(results, compileClass(tok)...)
+		compileClass(tok)
 	}
 	return results
 }
 
 // class = 'class' className '{' classVarDec* subroutineDec* '}'
-func compileClass(now token) []string {
-	results := []string{"<class>"}
-	results = append(results, compileToken(now))         // class
-	results = append(results, compileToken(nextToken())) // className
-	results = append(results, compileToken(nextToken())) // {
+func compileClass(now token) {
+	results = append(results, "<class>")
+	compileToken(now)         // class
+	compileToken(nextToken()) // className
+	compileToken(nextToken()) // '{'
 	tok := nextToken()
 	for tok.str != RBRACE {
 		if tok.str == "static" || tok.str == "field" {
-			classVarDec := compileClassVarDec(tok)
-			results = append(results, classVarDec...)
+			compileClassVarDec(tok)
 		}
 		if isSubroutine(tok.str) {
-			subroutine := compileSubroutine(tok)
-			results = append(results, subroutine...)
+			compileSubroutine(tok)
 		}
 		tok = nextToken()
 	}
-	results = append(results, compileToken(tok)) // }
+	compileToken(tok) // '}'
 	results = append(results, "</class>")
-	return results
 }
 
 // classVarDec = ('static' | 'field') type varName (',' varName)* ';'
-func compileClassVarDec(now token) []string {
-	results := []string{"<classVarDec>"}
-	results = append(results, compileToken(now))         // ('static' | 'field')
-	results = append(results, compileToken(nextToken())) // type
-	results = append(results, compileToken(nextToken())) // varName
+func compileClassVarDec(now token) {
+	results = append(results, "<classVarDec>")
+	compileToken(now)         // ('static' | 'field')
+	compileToken(nextToken()) // type
+	compileToken(nextToken()) // varName
 	tok := nextToken()
 	for tok.str == COMMA {
-		results = append(results, compileToken(tok))         // ','
-		results = append(results, compileToken(nextToken())) // varName
+		compileToken(tok)         // ','
+		compileToken(nextToken()) // varName
 		tok = nextToken()
 	}
-	results = append(results, compileToken(tok)) // ';'
+	compileToken(tok) // ';'
 	results = append(results, "</classVarDec>")
-	return results
 }
 
 func isSubroutine(str string) bool {
@@ -62,64 +65,61 @@ func isSubroutine(str string) bool {
 // subroutineDec = ('construct' | 'function' | 'method')
 //									('void' | type) subroutineName '(' parameterList ')'
 //									subroutineBody
-func compileSubroutine(now token) []string {
-	results := []string{"<subroutineDec>"}
-	results = append(results, compileToken(now))         // subroutineDec
-	results = append(results, compileToken(nextToken())) // type
-	results = append(results, compileToken(nextToken())) // subroutineName
+func compileSubroutine(now token) {
+	results = append(results, "<subroutineDec>")
+	compileToken(now)         // subroutineDec
+	compileToken(nextToken()) // type
+	compileToken(nextToken()) // subroutineName
 
-	results = append(results, compileToken(nextToken())) // (
-	paramList, tok := compileParameterList(nextToken())
-	results = append(results, paramList...)
-	results = append(results, compileToken(tok)) // )
+	compileToken(nextToken()) // '('
+	tok := compileParameterList(nextToken())
+	compileToken(tok) // ')'
 
 	// subroutineBody = '{' varDec* statements '}'
 	results = append(results, "<subroutineBody>")
-	results = append(results, compileToken(nextToken())) // {
+	compileToken(nextToken()) // '{'
 	tok1 := nextToken()
 	for tok1.str == "var" {
 		results = append(results, "<varDec>")
 		// varDec = 'var' type varName (',' varName)* ';'
-		results = append(results, compileToken(tok1))        // var
-		results = append(results, compileToken(nextToken())) // type
-		results = append(results, compileToken(nextToken())) // varName
+		compileToken(tok1)        // var
+		compileToken(nextToken()) // type
+		compileToken(nextToken()) // varName
 		tok2 := nextToken()
 		for tok2.str == COMMA {
-			results = append(results, compileToken(tok2))        // ','
-			results = append(results, compileToken(nextToken())) // varName
+			compileToken(tok2)        // ','
+			compileToken(nextToken()) // varName
 			tok2 = nextToken()
 		}
-		results = append(results, compileToken(tok2)) // ';'
+		compileToken(tok2) // ';'
 		tok1 = nextToken()
 		results = append(results, "</varDec>")
 	}
-	stmts, tok3 := compileStatements(tok1)
-	results = append(results, stmts...)
-	results = append(results, compileToken(tok3)) // }
+	tok3 := compileStatements(tok1)
+	compileToken(tok3) // '}'
 	results = append(results, "</subroutineBody>")
 
 	results = append(results, "</subroutineDec>")
-	return results
 }
 
 // parameterList = ((type varName) (',' type varName)* )?
-func compileParameterList(now token) ([]string, token) {
-	results := []string{"<parameterList>"}
+func compileParameterList(now token) token {
+	results = append(results, "<parameterList>")
 	if now.str == RPAREN {
 		results = append(results, "</parameterList>")
-		return results, now
+		return now
 	}
-	results = append(results, compileToken(now))         // type
-	results = append(results, compileToken(nextToken())) // varName
+	compileToken(now)         // type
+	compileToken(nextToken()) // varName
 	tok := nextToken()
 	for tok.str == COMMA {
-		results = append(results, compileToken(tok))         // ','
-		results = append(results, compileToken(nextToken())) // type
-		results = append(results, compileToken(nextToken())) // varName
+		compileToken(tok)         // ','
+		compileToken(nextToken()) // type
+		compileToken(nextToken()) // varName
 		tok = nextToken()
 	}
 	results = append(results, "</parameterList>")
-	return results, tok
+	return tok
 }
 
 func compileVarDec() {
@@ -127,8 +127,8 @@ func compileVarDec() {
 }
 
 // statements = statement*
-func compileStatements(now token) ([]string, token) {
-	results := []string{"<statements>"}
+func compileStatements(now token) token {
+	results = append(results, "<statements>")
 	// statement = letStatement | ifStatement |
 	// 						 whileStatement | doStatement |
 	// 						 returnStatement
@@ -137,15 +137,15 @@ func compileStatements(now token) ([]string, token) {
 	for {
 		switch tok.str {
 		case LET:
-			results = append(results, compileLet(tok)...)
+			compileLet(tok)
 		case IF:
-			results = append(results, compileIf(tok)...)
+			compileIf(tok)
 		case WHILE:
-			results = append(results, compileWhile(tok)...)
+			compileWhile(tok)
 		case DO:
-			results = append(results, compileDo(tok)...)
+			compileDo(tok)
 		case RETURN:
-			results = append(results, compileReturn(tok)...)
+			compileReturn(tok)
 		default:
 			loop = false
 		}
@@ -155,127 +155,111 @@ func compileStatements(now token) ([]string, token) {
 		tok = nextToken()
 	}
 	results = append(results, "</statements>")
-	return results, tok
+	return tok
 }
 
 // doStatement = 'do' subroutineCall ';'
-func compileDo(now token) []string {
-	results := []string{"<doStatement>"}
-	results = append(results, compileToken(now))         // do
-	results = append(results, compileToken(nextToken())) // identifier
+func compileDo(now token) {
+	results = append(results, "<doStatement>")
+	compileToken(now)         // do
+	compileToken(nextToken()) // identifier
 	tok := nextToken()
 	if tok.str == LPAREN {
 		// subroutineCall = subroutineName '(' exprList ')'
-		results = append(results, compileToken(tok)) // (
-		exprList, next := compileExpressionList(nextToken())
-		results = append(results, exprList...)
-		results = append(results, compileToken(next)) // )
+		compileToken(tok) // '('
+		next := compileExpressionList(nextToken())
+		compileToken(next) // ')'
 	}
 	if tok.str == PERIOD {
 		// subroutineCall = (className | varName) '.' subroutineName
 		// 									'(' exprList ')'
-		results = append(results, compileToken(tok))         // .
-		results = append(results, compileToken(nextToken())) // identifier
-		results = append(results, compileToken(nextToken())) // (
-		exprList, next := compileExpressionList(nextToken())
-		results = append(results, exprList...)
-		results = append(results, compileToken(next)) // )
+		compileToken(tok)         // '.'
+		compileToken(nextToken()) // identifier
+		compileToken(nextToken()) // '('
+		next := compileExpressionList(nextToken())
+		compileToken(next) // ')'
 	}
-	results = append(results, compileToken(nextToken())) // ;
+	compileToken(nextToken()) // ';'
 
 	results = append(results, "</doStatement>")
-	return results
 }
 
 // letstatement = 'let' varName ('[' expr ']')? '=' expr ';'
-func compileLet(now token) []string {
-	results := []string{"<letStatement>"}
-	results = append(results, compileToken(now))         // let
-	results = append(results, compileToken(nextToken())) // varName
+func compileLet(now token) {
+	results = append(results, "<letStatement>")
+	compileToken(now)         // let
+	compileToken(nextToken()) // varName
 	tok := nextToken()
 	if tok.str == LBRACKET {
-		results = append(results, compileToken(tok)) // [
-		expr, tok1 := compileExpression(nextToken())
-		results = append(results, expr...)
-		results = append(results, compileToken(tok1)) // ]
+		compileToken(tok) // '['
+		tok1 := compileExpression(nextToken())
+		compileToken(tok1) // ']'
 		tok = nextToken()
 	}
-	results = append(results, compileToken(tok)) // =
-	expr, tok := compileExpression(nextToken())
-	results = append(results, expr...)
-	results = append(results, compileToken(tok)) // ;
+	compileToken(tok) // '='
+	tok1 := compileExpression(nextToken())
+	compileToken(tok1) // ';'
 
 	results = append(results, "</letStatement>")
-	return results
 }
 
 // whileStatement = 'while' '(' expr ')' '{' statements '}'
-func compileWhile(now token) []string {
-	results := []string{"<whileStatement>"}
-	results = append(results, compileToken(now))         // while
-	results = append(results, compileToken(nextToken())) // (
-	expr, tok := compileExpression(nextToken())
-	results = append(results, expr...)
-	results = append(results, compileToken(tok))         // )
-	results = append(results, compileToken(nextToken())) // {
-	stmts, tok := compileStatements(nextToken())
-	results = append(results, stmts...)
-	results = append(results, compileToken(tok)) // }
+func compileWhile(now token) {
+	results = append(results, "<whileStatement>")
+	compileToken(now)         // 'while'
+	compileToken(nextToken()) // '('
+	tok := compileExpression(nextToken())
+	compileToken(tok)         // ')'
+	compileToken(nextToken()) // '{'
+	tok = compileStatements(nextToken())
+	compileToken(tok) // '}'
 	results = append(results, "</whileStatement>")
-	return results
 }
 
 // returnStatement = 'return' expr? ';'
-func compileReturn(now token) []string {
-	results := []string{"<returnStatement>"}
-	results = append(results, compileToken(now)) // return
+func compileReturn(now token) {
+	results = append(results, "<returnStatement>")
+	compileToken(now) // 'return'
 	tok := nextToken()
 	if tok.str != SEMICOLON {
-		expr, tok1 := compileExpression(tok)
-		results = append(results, expr...)
-		tok = tok1
+		tok = compileExpression(tok)
 	}
-	results = append(results, compileToken(tok)) // ';'
+	compileToken(tok) // ';'
 	results = append(results, "</returnStatement>")
-	return results
 }
 
 // ifStatement = 'if' '(' expr ')' '{' statements '}'
 // 							 ('else' '{' statements '}')?
-func compileIf(now token) []string {
-	results := []string{"<ifStatement>"}
-	results = append(results, compileToken(now))         // if
-	results = append(results, compileToken(nextToken())) // '('
-	expr, tok := compileExpression(nextToken())
-	results = append(results, expr...)
-	results = append(results, compileToken(tok))         // ')'
-	results = append(results, compileToken(nextToken())) // '{'
-	stmts, tok := compileStatements(nextToken())
-	results = append(results, stmts...)
-	results = append(results, compileToken(tok)) // '}'
+func compileIf(now token) {
+	results = append(results, "<ifStatement>")
+	compileToken(now)         // 'if'
+	compileToken(nextToken()) // '('
+	tok := compileExpression(nextToken())
+	compileToken(tok)         // ')'
+	compileToken(nextToken()) // '{'
+	tok = compileStatements(nextToken())
+	compileToken(tok) // '}'
 	if readNextToken().str == ELSE {
-		results = append(results, compileToken(nextToken())) // else
-		results = append(results, compileToken(nextToken())) // '{'
-		stmts, tok := compileStatements(nextToken())
-		results = append(results, stmts...)
-		results = append(results, compileToken(tok)) // '}'
+		compileToken(nextToken()) // 'else'
+		compileToken(nextToken()) // '{'
+		tok = compileStatements(nextToken())
+		compileToken(tok) // '}'
 	}
 	results = append(results, "</ifStatement>")
-	return results
 }
 
 // expr = term (op term)*
-func compileExpression(now token) ([]string, token) {
-	results := []string{"<expression>"}
-	results = append(results, compileTerm(now)...) // term
+func compileExpression(now token) token {
+	results = append(results, "<expression>")
+	compileTerm(now) // term
 	tok := nextToken()
 	for isOp(tok.str) {
-		results = append(results, compileToken(tok))           // op
-		results = append(results, compileTerm(nextToken())...) // term
+		compileToken(tok)        // op
+		compileTerm(nextToken()) // term
 		tok = nextToken()
 	}
 	results = append(results, "</expression>")
-	return results, tok
+	return tok
 }
 
 func isOp(str string) bool {
@@ -295,84 +279,78 @@ func isUnaryOp(str string) bool {
 // term = integerConstant | stringConstant | keywordConstant
 // 				| varName | varName '[' expr ']' | subroutineCall
 // 				| '(' expr ')' | unaryOp term
-func compileTerm(now token) []string {
-	results := []string{"<term>"}
+func compileTerm(now token) {
+	results = append(results, "<term>")
 	if now.kind == IDENTIFIER {
-		results = append(results, compileToken(now))
+		compileToken(now)
 		if readNextToken().str == LBRACKET {
 			// varName '[' expr ']'
-			results = append(results, compileToken(nextToken())) // [
-			expr, tok := compileExpression(nextToken())
-			results = append(results, expr...)
-			results = append(results, compileToken(tok)) // ]
+			compileToken(nextToken()) // '['
+			tok := compileExpression(nextToken())
+			compileToken(tok) // ']'
 		}
 		if readNextToken().str == LPAREN {
 			// subroutineCall = subroutineName '(' exprList ')'
-			results = append(results, compileToken(nextToken())) // (
-			exprList, next := compileExpressionList(nextToken())
-			results = append(results, exprList...)
-			results = append(results, compileToken(next)) // )
+			compileToken(nextToken()) // '('
+			next := compileExpressionList(nextToken())
+			compileToken(next) // ')'
 
 		}
 		if readNextToken().str == PERIOD {
 			// subroutineCall = (className | varName) '.' subroutineName
 			// 									'(' exprList ')'
-			results = append(results, compileToken(nextToken())) // .
-			results = append(results, compileToken(nextToken())) // subroutineName
-			results = append(results, compileToken(nextToken())) // (
-			exprList, next := compileExpressionList(nextToken())
-			results = append(results, exprList...)
-			results = append(results, compileToken(next)) // )
+			compileToken(nextToken()) // '.'
+			compileToken(nextToken()) // subroutineName
+			compileToken(nextToken()) // '('
+			next := compileExpressionList(nextToken())
+			compileToken(next) // ')'
 		}
 
 	} else if isUnaryOp(now.str) {
-		results = append(results, compileToken(now))           // uparyOp
-		results = append(results, compileTerm(nextToken())...) // term
+		compileToken(now)        // uparyOp
+		compileTerm(nextToken()) // term
 	} else if now.str == LPAREN {
 		// '(' expr ')'
-		results = append(results, compileToken(now)) // (
-		expr, tok := compileExpression(nextToken())
-		results = append(results, expr...)
-		results = append(results, compileToken(tok)) // (
+		compileToken(now) // '('
+		tok := compileExpression(nextToken())
+		compileToken(tok) // ')'
 	} else {
-		results = append(results, compileToken(now))
+		compileToken(now)
 	}
 	results = append(results, "</term>")
-	return results
 }
 
 // exprList = (expr (',' expr)* )?
-func compileExpressionList(now token) ([]string, token) {
-	results := []string{"<expressionList>"}
+func compileExpressionList(now token) token {
+	results = append(results, "<expressionList>")
 	if now.str == RPAREN {
 		results = append(results, "</expressionList>")
-		return results, now
+		return now
 	}
-	expr, tok1 := compileExpression(now)
-	results = append(results, expr...)
+	tok1 := compileExpression(now)
 
 	tok := tok1
 	for tok.str == COMMA {
-		results = append(results, compileToken(tok)) // ,
-		expr, tok2 := compileExpression(nextToken())
-		results = append(results, expr...)
+		compileToken(tok) // ','
+		tok2 := compileExpression(nextToken())
 		tok = tok2
 	}
 	results = append(results, "</expressionList>")
-	return results, tok
+	return tok
 }
 
-func compileToken(tok token) string {
+func compileToken(tok token) {
 	kind := string(tok.kind)
-	str := tok.str
-	return "<" + kind + "> " + str + " </" + kind + ">"
+	str := "<" + kind + "> " + tok.str + " </" + kind + ">"
+	results = append(results, str)
 }
 
 func compileTokenList(toks []token) []string {
-	results := []string{"<tokens>"}
+	initializeCompileEngine()
+	tokens = toks
+	results = append(results, "<tokens>")
 	for _, tok := range toks {
-		str := compileToken(tok)
-		results = append(results, str)
+		compileToken(tok)
 	}
 	results = append(results, "</tokens>")
 	return results
